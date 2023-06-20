bda5687dde9eca88e74f5287f25701d935b395e8
Ricardo Beaujon <rbeaujon77@gmail.com>
2023-06-20 12:37:18 +0300

    feat: added the counter display

---
+++ src/components/createCounter/createCounter.ts
@@ -0,0 +1,21 @@
+const createCounter = (app: PIXI.Application): [PIXI.Sprite, PIXI.Text] => {
+  const counter = PIXI.Sprite.from('./src/graphics/gameplay/counter.png');
+  counter.width = app.screen.width * 0.15;
+  counter.height = app.screen.height * 0.1;
+  counter.anchor.set(0.5);
+  counter.x = app.screen.width / 2;
+  counter.y = app.screen.height * 0.94;
+  app.stage.addChild(counter);
+
+  // Show the initial counter
+  const counterValue = new PIXI.Text("0");
+  counterValue.anchor.set(0.5);
+  counterValue.style.fontFamily = "Arial";
+  counterValue.style.fontSize = 80;
+  counterValue.style.fill = "white";
+  counter.addChild(counterValue);
+
+  return [counter, counterValue];
+};
+
+export default createCounter;
