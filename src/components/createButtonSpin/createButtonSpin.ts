b2bf4301d06040df832da055c7dbbbeef7ecff52
Ricardo Beaujon <rbeaujon77@gmail.com>
2023-06-20 12:36:14 +0300

    feat: added the spin button

---
+++ src/components/createButtonSpin/createButtonSpin.ts
@@ -0,0 +1,13 @@
+const createButtonSpin = (app: PIXI.Application): PIXI.Sprite => {
+  const buttonSpin = PIXI.Sprite.from('./src/graphics/gameplay/button_spin.png');
+  buttonSpin.width = app.screen.width * 0.1;
+  buttonSpin.height = app.screen.height * 0.2;
+  buttonSpin.anchor.set(0.5);
+  buttonSpin.x = app.screen.width * 0.85;
+  buttonSpin.y = app.screen.height * 0.5;
+  app.stage.addChild(buttonSpin);
+  
+  return buttonSpin;
+};
+
+export default createButtonSpin;
